e5be7f76cff7e2a26d9709120193d84684b238cc
Frederik Desmet <f.desmet@televic.com>
2020-09-09 15:43:19 +0200

    Removed TODO's and added some print statements to show more information to the user.

--- compare_config_csv.py
+++ compare_config_csv.py
@@ -2,10 +2,6 @@
     This script compares the CAF provided CSV with another version.
     The purpose for this action is the creation of the new config generation
     script.
-    TODO: Compare the Column names from both CSV files. DONE
-    TODO: Filter only the necessary Colums. DONE
-    TODO: Check the port row for duplicate vaulues. DONE
-    TODO: IF found remove the all EXCLUSIVE the original one.
 """
 
 from read_csv import DifferentiateCSV
@@ -20,14 +16,19 @@ def main():
     print()
     print("\033[4mOverview of all the unique headers:\033[0m")
     print()
+
     rcsv = DifferentiateCSV("consist_info.csv", "consist_info2.csv")
     rcsv.genereate_header_files()
+
     print("\n==============================================================\n")
     print("\033[4mOverview of the duplicate ports with their index:\033[0m")
     print()
+
     row_m = RowManipulation("consist_info.csv")
     row_m.show_duplicate_rows()
-    print()
+
+    print(f"\n\033[92mDuplicate rows have been succesfully removed.\033[0m")
+    print(f"The new file is generated: \033[4mconsist_info_final.csv\033[0m")
 
 
 if __name__ == "__main__":
