1110749b435be241b93aac228faa4f9e9c234944
Frederik Desmet <f.desmet@televic.com>
2020-09-09 15:39:55 +0200

    Removed TODO list, added dostrings on methods, and finally created a condition to check if the index list is empty.

--- row_manipulation.py
+++ row_manipulation.py
@@ -9,12 +9,6 @@
 import csv
 
 import pandas as pd
-'''
-    TODO: Select the correct column. (PORT)
-    TODO: Loop over the rows and print out duplicates with the same port number
-    TODO: Remove the duplicates, keep original (address, 248)
-    TODO: Create new CSV file with the unique rows. (check if only 3 rows less)
-'''
 
 
 class RowManipulation():
@@ -24,8 +18,13 @@ class RowManipulation():
         self.csv_file = csv_file
 
     def show_duplicate_rows(self):
+        """
+            This method creates a dataframe from the given file, loops over
+            it and if duplicates are found the shows the corresponding ones
+            with their index.
+        """
         # pd.set_option("display.max_rows", None, "display.max_columns", None)
-        data = pd.read_csv(self.csv_file)
+        data = pd.read_csv(self.csv_file, encoding="ISO-8859-1")
         previous_value = 0
         index = 0
         rows_to_remove = []
@@ -33,27 +32,33 @@ class RowManipulation():
             if previous_value == column:
                 print(
                     f"\trow line: \033[1m \033[31m{index}\033[0m \033[30m with"
-                    f"duplicate: \033[1m \033[31m{column}\033[0m \033[30m")
+                    f" duplicate: \033[1m \033[31m{column}\033[0m \033[30m")
                 rows_to_remove.append(index)
 
             index += 1
             previous_value = column
-
-            
-        self.remove_duplicate_rows(data, rows_to_remove)
-        self.write_new_csv()
+        if not rows_to_remove:
+            return print("no rows to delete.")
+        else:
+            self.remove_duplicate_rows(data, rows_to_remove)
+            self.write_new_csv()
 
     def remove_duplicate_rows(self, dataframe, index_from_row):
-        global data_list 
+        """
+            This method converts the dataframe to a list and uses the provided
+            indexes(in list form) to remove them from the list.
+        """
+        global data_list
         data_list = dataframe.values.tolist()
         for index in reversed(index_from_row):
-            print(f"removing the following row:\n{data_list[index]}")
             del data_list[index]
 
-    
     def write_new_csv(self):
+        """
+            This method creates a csv file(if it doesn't exist) and writes the
+            content of the global list.
+        """
         global data_list
-        with open('excluded_rows.csv', 'w') as writer_file:
+        with open('consist_info_final.csv', 'w', newline='') as writer_file:
             writer = csv.writer(writer_file)
             writer.writerows(data_list)
-
